90c50e4281819319d4233ee5dc728c0bd84e07a6
marijus <marijus@metasite.net>
2014-07-28 17:56:06 +0300

    no message

--- api/tradeideas/mockups/security.py
+++
@@ -1,8 +0,0 @@
-from .mockups import load_mockups
-
-
-def groupfinder(userid, request):
-    """Returns user groups specified in users.json mockup."""
-    users = load_mockups(request.registry.settings, 'users.json')
-    if userid in users:
-        return users[userid].get('groups', [])
